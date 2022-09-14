2e21a2e1481ef79a49f2cb5a39e407b9297c440c
Jhonatan alvarez™ <ardila098@gmail.com>
2022-09-14 10:04:47 -0500

    creacion del proyecto

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+
+import './index.css'
+import { PeliculasApp } from './PeliculasApp'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+
+
+    <PeliculasApp/>
+
+
+
+  </React.StrictMode>
+)
